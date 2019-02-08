14299ca4bdaa60c1950ca5264b75e2995d067523
Sallu-khiledar <sallu.33337@gmail.com>
2019-02-08 19:55:00 +0530

    Updates

---
+++ precache-manifest.b22054fa33b3577b62cdb8ffd1639bdc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2a5fb070de4fbc48cb63",
+    "url": "/hello1/static/css/main.73981ff1.chunk.css"
+  },
+  {
+    "revision": "2a5fb070de4fbc48cb63",
+    "url": "/hello1/static/js/main.2a5fb070.chunk.js"
+  },
+  {
+    "revision": "a07dad667d07a4b363d7",
+    "url": "/hello1/static/css/1.f83c5f5f.chunk.css"
+  },
+  {
+    "revision": "a07dad667d07a4b363d7",
+    "url": "/hello1/static/js/1.a07dad66.chunk.js"
+  },
+  {
+    "revision": "44180712f97de17b05e0",
+    "url": "/hello1/static/js/runtime~main.44180712.js"
+  },
+  {
+    "revision": "a33d366052f040b0935e20d3edc6e0c7",
+    "url": "/hello1/index.html"
+  }
+];
